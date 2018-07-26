e5ed8b1c0c4f687ce1d6a9395d0864b44d3ccaba
spreered <spreered@gmail.com>
2018-07-26 15:58:28 +0800

    modify card

--- asset/js/main.js
+++ asset/js/main.js
@@ -22,8 +22,7 @@
         <div class="card mb-2">
           <img class="card-img-top " src="${POSTER_URL}${data.image}" alt="Card image cap">
           <div class="card-body movie-item-body">
-            <h5 class="card-title">${data.title}</h5>
-            <p class="card-text">${data.description}</p>
+            <h6 class="card-title">${data.title}</h5>
           </div>
           <div class="card-footer">
           <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${data.id}">More</button>
